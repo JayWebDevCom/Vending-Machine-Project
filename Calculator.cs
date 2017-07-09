aa73c4383bb3526dfad179a5e076553770fe9076
JayWebDevCom <jaiye.s@gmail.com>
2017-07-09 15:50:39 +0100

    add Calculator and passing tests

--- Calculator.cs
+++ Calculator.cs
@@ -1,10 +1,19 @@
-﻿using System;
-namespace VendingMachineTechTest
-{
-    public class Calculator
-    {
-        public Calculator()
-        {
-        }
-    }
-}
+﻿using System; namespace VendingMachineTechTest {
+	public class Calculator
+	{
+		double acceptedCoin = 0.50;
+
+		public Calculator()
+		{
+		}
+
+		public double[] PaymentAndChange(Product product)
+		{
+			double cost = product.Price;
+			double coinsRequired = Math.Ceiling(cost / acceptedCoin);
+			double change = (coinsRequired * acceptedCoin) - cost;
+			double[] paymentInfo = { coinsRequired, change };
+
+	             return paymentInfo;
+		}
+	} }  
--- CalculatorTest.cs
+++ CalculatorTest.cs
@@ -1,13 +1,53 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 namespace VendingMachineTechTest
 {
     [TestFixture()]
-    public class NUnitTestClass
+    public class CalculatorTest
     {
         [Test()]
-        public void TestCase()
+        public void Instantitaion()
         {
+			Calculator calculator = new Calculator();
+			Assert.IsInstanceOf<Calculator>(calculator);
         }
+
+        [Test()]
+		public void CalculatesChangeAndCoinsRequired()
+		{
+			Calculator calculator = new Calculator();
+            Product juice = new Product("Juice", 0.80d);
+            double[] returned = calculator.PaymentAndChange(juice);
+            var returnedFormatted = Convert.ToDecimal(returned[1]);
+
+            Assert.AreEqual(2, returned[0]);
+			Assert.AreEqual(0.20, returnedFormatted);
+			Assert.AreEqual(2, returned.Length);
+		}
+
+		[Test()]
+		public void CalculatesChangeAndCoinsRequiredTwo()
+		{
+			Calculator calculator = new Calculator();
+			Product biscuits = new Product("Bisuits", 0.10d);
+			double[] returned = calculator.PaymentAndChange(biscuits);
+			
+            Assert.AreEqual(1, returned[0]);
+			Assert.AreEqual(0.40, returned[1]);
+			Assert.AreEqual(2, returned.Length);
+		}
+
+		[Test()]
+		public void CalculatesChangeAndCoinsRequiredThree()
+		{
+			Calculator calculator = new Calculator();
+			Product biscuits = new Product("Chocolate", 0.50);
+			double[] returned = calculator.PaymentAndChange(biscuits);
+
+			Assert.AreEqual(1, returned[0]);
+			Assert.AreEqual(0, returned[1]);
+			Assert.AreEqual(2, returned.Length);
+		}
     }
 }
